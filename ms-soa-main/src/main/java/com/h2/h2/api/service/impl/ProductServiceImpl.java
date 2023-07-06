4b5ea17cec3975fd8e211fbdd17a30d67905b1b4
Rey Gonzalez <rey.gonzalesdelriego@vallegrande.edu.pe>
2023-07-05 19:56:04 -0500

    blue

---
+++ ms-soa-main/src/main/java/com/h2/h2/api/service/impl/ProductServiceImpl.java
@@ -0,0 +1,37 @@
+package com.h2.h2.api.service.impl;
+
+import com.h2.h2.api.model.Funcionary;
+import com.h2.h2.api.respository.FuncionaryRepository;
+import com.h2.h2.api.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductServiceImpl implements ProductService {
+
+    @Autowired
+    private FuncionaryRepository funcionaryRepository;
+
+    @Override
+    public List<Funcionary> listados() {
+        return funcionaryRepository.findAll();
+    }
+
+    @Override
+    public Funcionary crear(Funcionary funcionary) {
+        return funcionaryRepository.save(funcionary);
+    }
+
+    @Override
+    public Funcionary update(Funcionary funcionary) {
+        return funcionaryRepository.save(funcionary);
+    }
+
+    @Override
+    public void delete(Integer id) {
+        funcionaryRepository.deleteById(id);
+    }
+
+}
